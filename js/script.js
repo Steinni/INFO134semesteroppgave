267e87189e26137c13080f3eab599b9141ca67e3
Steinni <35690651+Steinni@users.noreply.github.com>
2018-04-25 08:15:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,20 +4,18 @@
 var url;
 function request(url, callback){
 var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function(e){
+xhr.open("GET", url, true);
+xhr.onreadystatechange = function(){
   if(xhr.readyState ==4){
     if(xhr.status == 200){
-      var jsonFile = JSON.parse(xhr.responseText);
-      callback(jsonFile);
+      var responseJSON = JSON.parse(xhr.responseText);
+      callback(responseJSON);
     }else{
       callback(null);
     }
   }
 
     }
-    xhr.onload = request;
-    xhr.open("GET", url, true);
     xhr.send();
   }
 }
