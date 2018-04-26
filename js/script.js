b8dedc4d0763538e50f1c19de99fb257dda3c699
ittapopo <35119873+ittapopo@users.noreply.github.com>
2018-04-26 12:42:52 +0200

    Update script.js
    lagt til map skit

--- js/script.js
+++ js/script.js
@@ -18,3 +18,20 @@ xhr.onreadystatechange = function(){
     }
     xhr.send();
   }
+
+// i bunnen av checkboc søk. OPS kan mangle } bracket.
+var map = new google.maps.Map(document.getElementById('map'), {
+    			 zoom: 14,
+    			 center: new google.maps.LatLng(60.395025, 5.325094),
+		 });
+
+	    	for (var i = 0; i < json.length; i++) {
+				var tJson = toaletter.entries[i];
+	    	  	var marker = new google.maps.Marker({
+	        	position: {lat: parseFloat(tJson.latitude), lng: parseFloat(tJson.longitude)},
+	        	map: map,
+	 				 	label: tJson.id, 			// angir en label på markers med IDn til toalettene
+						title: tJson.plassering
+
+	      });
+	 }
