3d085560c9fe33289d35352d0bc83246090a2e9d
DanielHenstein <daniel@henstein.no>
2018-04-30 12:21:26 +0200

    added favorittlekeplass

---
+++ js/favoritt.js
@@ -0,0 +1,25 @@
+var lekeplass;
+var url = "https://hotell.difi.no/api/json/bergen/lekeplasser?";
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url, true);
+xhr.onreadystatechange = function(){
+	if(xhr.status == 200 && xhr.readyState == 4){
+			lekeplass = JSON.parse(xhr.responseText).entries;
+			jsonOption(lekeplass);
+	}
+}
+xhr.send()
+
+var option = document.getElementById("lekeplassoption");
+function jsonOption(arr) {
+	var option = document.getElementById("lekeplassoption");
+	for(var i = 0; i < arr.length; i++){
+		option.innerHTML = option.innerHTML + '<option value"' + arr[i].id + '">' + arr[i].navn + '</option>';
+	}
+}
+
+function myoption(){
+	var selected = document.getElementById("lekeplassoption").value;
+	document.getElementById("favoritt").innerHTML = "Din valgte lekeplassen: " + selected;
+
+}
