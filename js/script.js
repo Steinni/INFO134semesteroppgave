410c414c6e1dd36479071bd9046bfd3260ae0215
ittapopo <35119873+ittapopo@users.noreply.github.com>
2018-05-01 13:45:26 +0200

    lagt til funksjon som splitter og intparser string
    isOpen er ikke ferdig, bare første condition (if day.value = 6...)blir utførst

--- js/script.js
+++ js/script.js
@@ -61,3 +61,42 @@ function googleMaps(json) {
     })(marker, i));
 }
 }
+
+// Kode for å se om toaletter.entries er åpne
+function splitNparse(string) {								// Funksjon til å splitte og parse toaletter.entries[i].tid_.... 
+    var tidString = string; 								//Tar en json entry string
+    var splitString = tidString.split(/[.-]+/); 			//Splitter string til ny array f.eks
+    var numbers = [];										//"09.00 - 21.00" blir "09", "00", "21", "00"
+
+    	for(i = 0; i < splitString.length; i++) {			//itererer gjennom ny array og parse string til int
+    	var num = parseInt(splitString[i], 10);				//f. eks 9, 0, 21, 0 (dobbel null er unødvendig som int)
+    	numbers.push(num);
+    
+    	}
+    console.log(splitString);								//consolelogger string så man kan se det er rett.
+    return numbers;											//returnerer int array.
+}
+
+function isOpen(toalett) {									//	Funksjon for å se om toaletter.entries er åpne
+	var toilet = toalett;									//Tar et json objekt
+	var date = new Date();									//Bruker date funksjonen for å hente ut verdier
+	var day = date.getDay();								//Dager; man = 1, tir = 2 ... søn = 0
+	var hour = date.getHours();								//Timer
+	var minute = date.getMinutes();							//Minutter
+	var open = false;										//Binær variabel som tilsvarer open og stengt.
+		
+		if (day.value = 6) {										
+			var q = splitNparse(toilet.tid_lordag);
+			console.log(q); // open = true;
+		}
+		if (day.value = 0) {
+			var q = splitNparse(toilet.tid_sondag);
+			console.log(q); // open = true;
+		}
+		else if (day.value <= 5 && day.value > 0) {
+			var q = splitNparse(toilet.tid_hverdag);
+			console.log(q); // open = true;
+		}
+		
+
+}
